3bf7c94f01e793ed6291a0136177d397f43f97d6
neuronet <neuronet.it@gmail.com>
2017-02-25 21:34:35 +0100

    setTimeout removed

--- promiseloop.js
+++ promiseloop.js
@@ -14,9 +14,7 @@ module.exports = function promiseLoop(promiseImplementation){
         });
       }else{
         p.then(()=>{
-          setTimeout(()=>{
-            finalFn();
-          },0);
+          finalFn();
         });
       }
     }
